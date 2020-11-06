072793e3bc25f51a3e329e897bea138741020cc7
Yael Chavoya <yaelchavoya@gmail.com>
2020-11-06 11:07:13 -0600

    Agregar codificación y decodificación

--- huffman.py
+++ huffman.py
@@ -74,6 +74,7 @@ def _assign_codes(root):
 def regular_binary(string: str):
     return ''.join(format(ord(x), 'b') for x in string)
 
+
 def compress(string: str):
 
     frecuencias = {}
@@ -92,4 +93,41 @@ def compress(string: str):
     for char in string:
         compressed = compressed + codes[char]
 
-    return compressed
+    return compressed, codes
+
+
+def encode_key(codes: Dict[str, str]):
+    encoded = ''
+    for char, code in codes.items():
+        char_hex = hex(int(regular_binary(char),2))
+        code_hex = hex(int(code,2))
+        encoded += char_hex[2:] + '.' + code_hex[2:] + ':'
+    return encoded[:-1]
+
+
+def decode_key(code: str) -> Dict[str, str]:
+    decoded = {}
+    codes = code.split(':')
+    for cd in codes:
+        item = cd.split('.')
+        char = chr(int(item[0], 16))
+        code_b = bin(int(item[1], 16))[2:]
+        decoded[char] = code_b
+    return decoded
+
+
+def decompress(binstring: str, codes: Dict[str, str]):
+    fullstr = ''
+    capture = ''
+    matches = []
+    for bit in binstring:
+        capture += bit
+        matches.clear()
+        for char, code in codes.items():
+            if capture == code[0:len(capture)]:
+                matches.append(char)
+        print(capture, matches)
+        if len(matches) == 1:
+            fullstr += matches[0]
+            capture = ''
+    return fullstr
